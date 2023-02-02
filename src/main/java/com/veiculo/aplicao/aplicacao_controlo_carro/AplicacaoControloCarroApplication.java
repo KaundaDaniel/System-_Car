9a49aea52a5d6ea6a2bc7f3b8ed8b4dd3f0081db
dannydagamaisutic@gmail.com <dannydagamaisutic@gmail.com>
2023-02-02 12:06:26 +0100

    Setup read

---
+++ src/main/java/com/veiculo/aplicao/aplicacao_controlo_carro/AplicacaoControloCarroApplication.java
@@ -0,0 +1,13 @@
+package com.veiculo.aplicao.aplicacao_controlo_carro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacaoControloCarroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacaoControloCarroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/veiculo/aplicao/aplicacao_controlo_carro/AplicacaoControloCarroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veiculo.aplicao.aplicacao_controlo_carro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacaoControloCarroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
